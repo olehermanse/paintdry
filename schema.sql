0f5b69f4f6724d84e82a2d51b67eed2c7b417d9e
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2023-11-12 02:56:34 +0100

    Added schema.sql file to set up PG tables
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS index (
+  entry_id serial PRIMARY KEY,
+  entry TEXT NOT NULL UNIQUE,
+  metadata TEXT DEFAULT NULL,
+  urls TEXT[] DEFAULT NULL,
+  first_seen TIMESTAMP DEFAULT NOW(),
+  last_seen TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS links (
+  src TEXT NOT NULL,
+  text TEXT NOT NULL,
+  dst TEXT NOT NULL
+);
+
+INSERT INTO index (entry)
+  VALUES('test_entry')
+  ON CONFLICT (entry)
+  DO UPDATE SET last_seen = NOW();
