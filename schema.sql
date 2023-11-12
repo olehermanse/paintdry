52aad3cf4ba54437fc84b1a77a028c9ca8a6ba16
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2023-11-12 03:42:52 +0100

    Added table for history
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- schema.sql
+++ schema.sql
@@ -7,6 +7,15 @@ CREATE TABLE IF NOT EXISTS index (
   last_seen TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS history (
+  entry_id serial PRIMARY KEY,
+  entry TEXT NOT NULL,
+  metadata TEXT DEFAULT NULL,
+  urls TEXT[] DEFAULT NULL,
+  first_seen TIMESTAMP DEFAULT NOW(),
+  last_seen TIMESTAMP DEFAULT NOW()
+);
+
 CREATE TABLE IF NOT EXISTS links (
   src TEXT NOT NULL,
   text TEXT NOT NULL,
