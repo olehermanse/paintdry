323ba1b45ce788c4c2f57720ab698c0eb4555019
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2024-02-20 00:06:08 +0100

    Fixed issues with URLs
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- lookup/modules/http.py
+++ lookup/modules/http.py
@@ -3,8 +3,46 @@ import copy
 import requests
 
 
+class HTTPGet:
+    """Abstraction / encapsulation of requests.get()
+
+    Expose only the properties we actually need.
+    """
+
+    def __init__(self, url):
+        self._r = requests.get(url, allow_redirects=False)
+        self._url = url
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def status_code(self):
+        return self._r.status_code
+
+    @property
+    def redirect_location(self):
+        return self._r.headers.get("Location")
+
+
+def strip_prefix(inp, options, n):
+    if n == 0:
+        return inp
+    for prefix in options:
+        if inp.startswith(prefix):
+            return strip_prefix(inp[len(prefix) :], options, n - 1)
+    return inp
+
+
+def simplify_url(url):
+    if url.endswith("/"):
+        return simplify_url(url[0:-1])
+    return strip_prefix(url, ("http://", "https://"), 1)
+
+
 def is_root_url(url):
-    return url.endswith((".io", ".com", ".tech", ".io/", ".com/", ".tech/"))
+    return "/" not in url
 
 
 def is_https_url(url):
@@ -26,17 +64,16 @@ def module_http(entry):
 
     while entry["identifier"].endswith("/"):
         entry["identifier"] = entry["identifier"][0:-1]
-    url = entry["identifier"]
-    code, redirect_location = get_status_code_and_location(url)
+    r = HTTPGet(entry["identifier"])
 
     entry["type"] = "status"
-    entry["value"] = code
+    entry["value"] = r.status_code
     results.append(copy.deepcopy(entry))
 
-    if redirect_location:
-        print(f"Website: {url} -> {redirect_location} ({code})")
+    if r.redirect_location:
+        print(f"Website: {r.url} -> {r.redirect_location} ({r.status_code})")
         entry["type"] = "redirect_location"
-        entry["value"] = redirect_location
+        entry["value"] = r.redirect_location
         results.append(copy.deepcopy(entry))
         discoveries.append(
             {
@@ -45,12 +82,12 @@ def module_http(entry):
             }
         )
     else:
-        print(f"Website: {url} -> {code}")
+        print(f"Website: {r.url} -> {r.status_code}")
 
-    if is_https_url(url) and is_root_url(url):
+    if is_https_url(r.url) and is_root_url(r.url):
         http_entry = {}
         http_entry["module"] = "http"
-        http_entry["identifier"] = https_to_http(url)
+        http_entry["identifier"] = https_to_http(r.url)
         discoveries.append(http_entry)
 
     return (results, discoveries)
